3304daee0935c74a28b8501e01966b491208a65d
mcdomingo <mcdomingo@dons.usfca.edu>
2018-09-25 15:51:50 -0700

    code cleanup complete

--- cutout_programs/py/open_fits.py
+++ cutout_programs/py/open_fits.py
@@ -1,7 +1,7 @@
 import numpy as np
 from astropy.io import fits
 
-fits_catalog_keys = {
+FITS_CATALOG_KEYS = {
     "brickname",
     "objid",
     "ra",
@@ -20,7 +20,7 @@ def extract_from_fits_file(fits_file, keys={}):
         data = hdul[1].data
         if not keys:
             keys = hdul[1].columns.names
-        return [{key: datum[key] for key in fits_catalog_keys} for datum in data]
+        return [{key: datum[key] for key in FITS_CATALOG_KEYS} for datum in data]
 
 
 def check_dict_key(dict1, dict2, key):
@@ -30,8 +30,8 @@ def check_dict_key(dict1, dict2, key):
     return False
 
 def match_two_fits_files(fits_file1, fits_file2, matching_keys):
-    fits1_vals = extract_from_fits_file(fits_file1, fits_catalog_keys)
-    fits2_vals = extract_from_fits_file(fits_file2, fits_catalog_keys)
+    fits1_vals = extract_from_fits_file(fits_file1, FITS_CATALOG_KEYS)
+    fits2_vals = extract_from_fits_file(fits_file2, FITS_CATALOG_KEYS)
 
     for i in range(min(len(fits1_vals), len(fits2_vals))):
         if not all(check_dict_key(fits1_vals[i], fits2_vals[i], key) for key in check_keys):
